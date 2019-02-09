30dddd2c9748ab68b52ea8ca05fa0785d8d19d3e
yanggao <gaoy2@rose-hulman.edu>
2019-02-09 14:59:50 -0500

    staged

--- app/src/main/java/edu/rose/snack/snackplus/DriverActivity.kt
+++ app/src/main/java/edu/rose/snack/snackplus/DriverActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import edu.rose.snack.snackplus.driver.landing.DriverLandingFragment
